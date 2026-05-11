a1329b94e8d763369d260ed23eed48753b5f49d6
Elminsst <130235860+Elminsst@users.noreply.github.com>
2026-05-12 01:20:40 +0800

    Add key-value store functionality in fetch method
    Store a value in RPKV and retrieve it in the fetch method.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 export default {
   async fetch(request, env) {
 
-    return env.ASSETS.fetch(request)
+    await env.RPKV.put("hello", "world")
+
+    const value = await env.RPKV.get("hello")
+
+    return new Response(value)
 
   }
 }
