651d8c483986901686fb39788db8c3bc8f72e428
Elminsst <130235860+Elminsst@users.noreply.github.com>
2026-05-12 13:22:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
+const USERNAME = "Elmin"
+const PASSWORD = "qaz741"
+
 export default {
   async fetch(request, env) {
 
-    await env.RPKV.put("hello", "world")
+    const auth = request.headers.get("Authorization")
 
-    const value = await env.RPKV.get("hello")
+    // 没有登录
+    if (!auth) {
+      return new Response("Unauthorized", {
+        status: 401,
+        headers: {
+          "WWW-Authenticate": 'Basic realm="RP-Hub"'
+        }
+      })
+    }
 
-    return new Response(value)
+    const [scheme, encoded] = auth.split(" ")
+
+    // 不是 Basic
+    if (scheme !== "Basic") {
+      return new Response("Invalid auth", {
+        status: 401
+      })
+    }
+
+    // 解码账号密码
+    const decoded = atob(encoded)
+
+    const [user, pass] = decoded.split(":")
+
+    // 验证失败
+    if (
+      user !== USERNAME ||
+      pass !== PASSWORD
+    ) {
+      return new Response("Forbidden", {
+        status: 403
+      })
+    }
+
+    // 验证成功
+    return env.ASSETS.fetch(request)
 
   }
 }
