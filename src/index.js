637a7f0868e380b207a70500fedd06c602a086fe
Elminsst <130235860+Elminsst@users.noreply.github.com>
2026-05-12 13:40:22 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 export default {
+
   async fetch(request, env) {
 
-    return new Response(`
-      <h1>WORKER OK</h1>
-    `, {
-      headers: {
-        "Content-Type": "text/html"
-      }
-    })
+    const url = new URL(request.url)
+
+    // 强制测试 Worker 是否运行
+    if (url.pathname === "/test") {
+
+      return new Response("WORKER OK")
+
+    }
+
+    // 登录页
+    if (url.pathname === "/login") {
+
+      return new Response(`
+        <h1>LOGIN PAGE</h1>
+      `, {
+        headers: {
+          "Content-Type": "text/html"
+        }
+      })
+
+    }
+
+    // 所有其他请求
+    return env.ASSETS.fetch(request)
 
   }
+
 }
