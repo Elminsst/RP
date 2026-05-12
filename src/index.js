0e594a2968685f02a3938fe6aefa4bcebc18f012
Elminsst <130235860+Elminsst@users.noreply.github.com>
2026-05-12 13:43:49 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,27 +4,64 @@ export default {
 
     const url = new URL(request.url)
 
-    // 强制测试 Worker 是否运行
-    if (url.pathname === "/test") {
+    const cookie =
+      request.headers.get("Cookie") || ""
 
-      return new Response("WORKER OK")
-
-    }
+    const loggedIn =
+      cookie.includes("rp_auth=1")
 
     // 登录页
     if (url.pathname === "/login") {
 
       return new Response(`
-        <h1>LOGIN PAGE</h1>
+        <form method="POST" action="/auth">
+          <input name="password" type="password">
+          <button>登录</button>
+        </form>
       `, {
         headers: {
           "Content-Type": "text/html"
         }
       })
-
     }
 
-    // 所有其他请求
+    // 登录接口
+    if (
+      url.pathname === "/auth" &&
+      request.method === "POST"
+    ) {
+
+      const form =
+        await request.formData()
+
+      const password =
+        form.get("password")
+
+      if (password === "123456") {
+
+        return new Response(null, {
+          status: 302,
+          headers: {
+            "Location": "/",
+            "Set-Cookie":
+              "rp_auth=1; Path=/;"
+          }
+        })
+      }
+
+      return new Response("密码错误")
+    }
+
+    // 未登录
+    if (!loggedIn) {
+
+      return Response.redirect(
+        `${url.origin}/login`,
+        302
+      )
+    }
+
+    // 返回静态资源
     return env.ASSETS.fetch(request)
 
   }
